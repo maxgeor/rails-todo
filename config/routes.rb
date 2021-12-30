f5a0b079222ce378ba753fca1cbd7a10a99c1d3b
Max Georgopoulos <maxgeorgopoulos@Maxs-MacBook-Air.local>
2021-12-30 17:36:33 -0500

    Replaced Home with Todo scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :todos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-get 'home', to: 'home#index'
+  get 'todos', to: 'todos#index'
   # Defines the root path route ("/")
-  root "home#index"
+  root "todos#index"
 end
